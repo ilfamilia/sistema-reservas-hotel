b93553711588caf9007b64e74c6e86de75fef861
Abalbuena00 <balbuena.alexb@gmail.com>
2026-03-10 02:58:44 -0400

    Proyecto casi terminado, ajustar algunos detalles

--- src/hotel/Main.java
+++ src/hotel/Main.java
@@ -1,7 +1,21 @@
 package hotel;
 
+import hotel.consola.MenuPrincipal;
+import hotel.servicio.ServicioReserva;
+import hotel.servicio.ServicioDisponibilidad;
+import hotel.repositorio.memoria.RepositorioHabitacionEnMemoria;
+import hotel.repositorio.memoria.RepositorioReservaEnMemoria;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Sistema de reservas de hotel iniciado");
+        // Repositorios en memoria (ejemplo)
+        RepositorioHabitacionEnMemoria repoHabitacion = new RepositorioHabitacionEnMemoria();
+        RepositorioReservaEnMemoria repoReserva = new RepositorioReservaEnMemoria();
+
+        ServicioDisponibilidad servicioDisponibilidad = new ServicioDisponibilidad(repoHabitacion, repoReserva);
+        ServicioReserva servicioReserva = new ServicioReserva(repoReserva, repoHabitacion, servicioDisponibilidad);
+
+        MenuPrincipal menu = new MenuPrincipal(servicioReserva, servicioDisponibilidad);
+        menu.mostrar();
     }
 }
